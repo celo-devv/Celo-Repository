4e5d05690fd5ec1835da6bb0b1830d22dd379fc4
celo-devv <mhmnprofissional@gmail.com>
2024-08-14 13:13:10 -0300

    Add files via upload

---
+++ Exercicos - Traduçoes/Tradução_PTgolpC#/Program.cs
@@ -0,0 +1,8 @@
+﻿double qtdeReais, qtdeDolar;
+
+ Console.WriteLine("Escreva a quantidade de reais");
+ qtdeReais = double.Parse(Console.ReadLine());
+ qtdeDolar = qtdeReais / 5.45;
+ qtdeDolar = Math.Round(qtdeDolar, 2, MidpointRounding.AwayFromZero);
+Console.WriteLine($"Você tem {qtdeDolar} dolares");
+Console.ReadKey();
